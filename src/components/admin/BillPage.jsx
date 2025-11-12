8bbf7bdf1ce2f5d720a5062b5bcdcfa344d67c68
azhar-dev <azhar.webdev1@gmail.com>
2025-11-13 00:29:04 +0530

    Latest update with redux

--- src/components/admin/BillPage.jsx
+++ src/components/admin/BillPage.jsx
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import { motion } from "framer-motion"; // Optional: for modal animation
 
 const BillPage = ({ order, restaurantDetails, onClose }) => {
+  console.log(order)
   const billRef = useRef();
 
   const handlePrint = () => {
